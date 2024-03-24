252de258bb22a44ac1b162e50a565a9f5e3264fe
Papalitas09 <miaw.raja@gmail.com>
2024-03-24 14:38:44 +0700

    nih dit

--- forum.php
+++ forum.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 ?>
 
   <!-- form post -->
@@ -13,31 +15,45 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+
 <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.4/dist/flowbite.min.css" />
 
 <!-- This is an example component -->
-<div class="max-w-2xl mx-auto">
-    <form action="" method="">
+<div class="max-w-2xl mx-auto mt-8">
+    <form action="process.php" method="POST">
 	<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+    
+  <textarea id="message" rows="4" name="cp" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+  <div class="mt-8 flex justify-center">
 
-    <input type="submit" name="submit">
-    <a href="home.php">Cancel</a>
-	</p>
+  <!-- kondisi -->
+   <?php
+      if(isset($_GET['ubah'])){
+    ?>
+    <button type="submit" name="submit" value="edit" class="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+  data-ripple-light="true">Save</button>
+  <a href="home.php"  class="middle none center mr-4 rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+  data-ripple-light="true">Cancel</a>
+
+  <?php
+   } else {
+  ?>
+
+  <button type="submit" name="submit" value="add" class="middle none center mr-4 rounded-lg bg-blue-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+  data-ripple-light="true">Submit</button>
+  <a href="home.php"  class="middle none center mr-4 rounded-lg bg-red-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-red-500/20 transition-all hover:shadow-lg hover:shadow-red-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+  data-ripple-light="true">Cancel</a>
+
+  <?php
+   } ?>
+
+</div>
+    <!-- component juga -->
     <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
 </div>
 </form>
-
 </body>
 </html>
 
 
 
-  <!-- <div class=" hidden fixed flex flex-col justify-center items-center bg-slate-200/40 w-1/2 h-1/2 self-center rounded-lg backdrop-blur" id="formUpload">
-      <form class="flex flex-col justify-center items-center" action="./action/upload.php" method="post">       
-        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Caption</label>
-        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-        <input class="mb-6" type="file" accept=".jpg, .png, .jpeg, .webp">
-        <input type="submit" value="Upload">
-      </form>
-    </div> -->
---
+++ process.php
@@ -0,0 +1,56 @@
+<?php
+    include ("konek.php");
+
+        if(isset($_POST['submit'])){
+            if($_POST['submit'] == "add"){
+              add();
+            } else if ($_POST['submit'] == "edit"){
+              edit();
+            } else {
+              delete();
+            }
+          }
+        
+          function add(){
+            global $konek;
+            $caption = $_POST['cp'];
+        
+            $query= "INSERT INTO `post`(`caption`) VALUES ('$caption')";
+            $sql = mysqli_query($konek, $query);
+        
+            if($sql){
+              header("location: home.php");
+            } else {
+              echo "<script>alert('error')</script>";
+            }
+        
+        
+        
+        
+          }
+          function edit(){
+            echo "oke";
+          }
+          function delete(){
+            global $konek;
+            
+            $id = $_GET['hapus'];
+            
+            $query = "DELETE FROM `post` WHERE id='$id';";
+            $sql = mysqli_query($konek, $query);
+     
+                 if($sql){
+                     header("location: home.php");
+                 } else {
+                     echo $query;
+                 }
+          }
+          
+
+
+
+
+
+
+
+?>
