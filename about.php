57e6353fbb5cf2b66d776fa079668cdf1feb057f
name <youremail>
2024-03-26 14:48:14 +0700

    konztol

--- about.php
+++ about.php
@@ -6,39 +6,76 @@
   <title>Document</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="flex flex-col justify-center items-center h-screen bg-slate-900/10">
-  <h1 class="font-bold text-3xl mt-4">AUTHOR</h1>
-<div class="flex justify-center items-center h-screen flex-col -mt-16">
+<body class="flex flex-col justify-center items-center h-screen bg-slate-900/10 mx-10">
+  <h1 class="font-bold text-3xl mt-4"><b>AUTHOR</b></h1>
+  <div class="flex justify-around  flex-col mt-2 columns-5 grid grid-cols-5 gap-4 content-start">
+  
   <li class="text-sm leading-6">
     <figure class="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
       <blockquote class="mt-6 text-slate-700 dark:text-slate-300">
-        <p> BUSTREP BAU MENDING TELWIN, PHP BUSUK MENDING NODJEES</p>
+        <p>BENCI BOOTSRAP, PECINTA TAILWIND GARIS KERAS NIH BOSS</p>
       </blockquote>
       <figcaption class="flex items-center space-x-4"><img src="./img/botak.jpg" alt="" class="flex-none w-14 h-14 rounded-full object-cover" loading="lazy" decoding="async">
         <div class="flex-auto">
           <div class="text-base text-slate-900 font-semibold dark:text-slate-300">
             <a href="" tabindex="0"><span class="absolute inset-0"></span>Adit Mikroprocessor</a>
           </div>
-          <div class="mt-0.5 text-white">Remix &amp; React Training</div>
+          <div class="mt-0.5 text-white">Leader of team & Main contributors</div>
         </div>
       </figcaption>
     </figure>
   </li>
+
   <li class="text-sm leading-6">
     <figure class="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
       <blockquote class="mt-6 text-slate-700 dark:text-slate-300">
-        <p> BUSTREP BAU MENDING TELWIN, PHP BUSUK MENDING NODJEES</p>
+        <p>GA TAU MALES PENGEN MAKAN MIE AYAM PAKE ES MILO</p>
       </blockquote>
       <figcaption class="flex items-center space-x-4"><img src="./img/guest.png" alt="" class="flex-none w-14 h-14 rounded-full object-cover" loading="lazy" decoding="async">
         <div class="flex-auto">
           <div class="text-base text-slate-900 font-semibold dark:text-slate-300">
             <a href="" tabindex="0"><span class="absolute inset-0"></span>Satria Skeleton Gacor</a>
           </div>
-          <div class="mt-0.5 text-white">Remix &amp; React Training</div>
+          <div class="mt-0.5 text-white">Contributors</div>
         </div>
       </figcaption>
     </figure>
   </li>
+  
+  <li class="text-sm leading-6">
+    <figure class="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+      <blockquote class="mt-6 text-slate-700 dark:text-slate-300">
+        <p>SUKA SALTO DI KOLAM BERENANG PAKE BOXER</p>
+      </blockquote>
+      <figcaption class="flex items-center space-x-4"><img src="./img/guest.png" alt="" class="flex-none w-14 h-14 rounded-full object-cover" loading="lazy" decoding="async">
+        <div class="flex-auto">
+          <div class="text-base text-slate-900 font-semibold dark:text-slate-300">
+            <a href="" tabindex="0"><span class="absolute inset-0"></span>Dhavin HDMI</a>
+          </div>
+          <div class="mt-0.5 text-white">Contributors</div>
+        </div>
+      </figcaption>
+    </figure>
+  </li>
+  
+  <li class="text-sm leading-6">
+    <figure class="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+      <blockquote class="mt-6 text-slate-700 dark:text-slate-300">
+        <p>SUAMI MEMBER JKT48 DAN SUKA NONTONIN F1</p>
+      </blockquote>
+      <figcaption class="flex items-center space-x-4"><img src="./img/guest.png" alt="" class="flex-none w-14 h-14 rounded-full object-cover" loading="lazy" decoding="async">
+        <div class="flex-auto">
+          <div class="text-base text-slate-900 font-semibold dark:text-slate-300">
+            <a href="" tabindex="0"><span class="absolute inset-0"></span>Haikal Mediatek</a>
+          </div>
+          <div class="mt-0.5 text-white">Contributors</div>
+        </div>
+      </figcaption>
+    </figure>
+  </li>
+ 
+
+
   <li class="text-sm leading-6">
     <figure class="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
       <blockquote class="mt-6 text-slate-700 dark:text-slate-300">
@@ -49,12 +86,14 @@
           <div class="text-base text-slate-900 font-semibold dark:text-slate-300">
             <a href="" tabindex="0"><span class="absolute inset-0"></span>Erlang Suami Siesta</a>
           </div>
-          <div class="mt-0.5 text-white">Remix &amp; React Training</div>
+          <div class="mt-0.5 text-white">SEPUH & HELPER</div>
         </div>
       </figcaption>
     </figure>
-  </li> <br> <br> <br>
+  </li>
+  </div>
   
+  <div class="mt-8">
 <ol class="items-center sm:flex">
     <li class="relative mb-6 sm:mb-0">
         <div class="flex items-center">
@@ -66,9 +105,9 @@
             <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
         </div>
         <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.0.0</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">History Website</time>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">ABOUT OUR WEB</h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Feature Our Web</time>
+            <p class="text-base font-normal text-slate-950">Web Sosmed Abal-Abal yang sudah menggunakan CRUD(Create, Read, Update, Delete)</p>
         </div>
     </li>
     <li class="relative mb-6 sm:mb-0">
@@ -81,9 +120,9 @@
             <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
         </div>
         <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.0</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">About this Website</time>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">ABOUT OUR WEB</h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Waktu pembuatan</time>
+            <p class="text-base font-normal text-slate-950">Waktu pembuatan web kita dibuat sekitar 1 minggu++</p>
         </div>
     </li>
     <li class="relative mb-6 sm:mb-0">
@@ -96,15 +135,14 @@
             <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
         </div>
         <div class="mt-3 sm:pe-8">
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.3.0</h3>
-            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Isi apa kek udh gede</time>
-            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">ABOUT OUR WEB</h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Result</time>
+            <p class="text-base font-normal text-slate-950">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, voluptates.</p>
         </div>
     </li>
 </ol>
-
-
 </div>
+
 
 </body>
 </html>
